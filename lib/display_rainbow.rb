aa98253baa8fdc14ed8dc4157ebc1a4f325e8c46
Michael Wang <michwng@gmail.com>
2020-12-06 22:40:08 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,3 @@
 def display_rainbow(colors)
   puts "R: #{colors[0]}, O; #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
 end
-
-
-  colors = [0, 1, 2, 3, 4, 5, 6]
